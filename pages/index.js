931c685d765bc7841587b47f01ef2b775f5b6699
unknown <j.c.delafields@gmail.com>
2023-05-12 11:41:00 -0400

    links -> icons on home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { BsYoutube, BsInstagram, BsGithub} from "react-icons/bs";
 import useSound from 'use-sound';
 import drillSfx from '../public/sounds/drill.mp3';
 import keyboardSfx from '../public/sounds/keyboard.mp3';
@@ -45,25 +46,48 @@ export default function Home() {
         </div>
         <div></div>
         <div className="flex flex-col items-center sm:place-self-end sm:mr-12 sm:mb-24">
+          <div className='flex justify-between w-32 my-3'>
+            <a 
+              className="text-2xl font-bold text-myred hover:opacity-60"  
+              href="https://www.youtube.com/@supersecretworkshop"
+              target="_blank" rel="noreferrer"
+            >
+              <BsYoutube/>
+            </a>
+            <a 
+              className="text-2xl font-bold text-myred hover:opacity-60"  
+              href="https://www.instagram.com/supersecretworkshop/"
+              target="_blank" rel="noreferrer"
+            >
+              <BsInstagram/>
+            </a>
+            <a 
+                className="text-2xl font-bold text-myred hover:opacity-60"  
+                href="https://github.com/delafields"
+                target="_blank" rel="noreferrer"
+              >
+                <BsGithub/>
+            </a>
+          </div>
           <Link href="/experience">
             <a className="font-bold text-myred hover:opacity-60">
             work experience
             </a>
           </Link>
-          <a 
+          {/* <a 
             className="my-1 font-bold text-myred hover:opacity-60"
             href="https://twitter.com/DeLaFields"
             target="_blank" rel="noreferrer"  
           >
             twitter
-          </a>
-          <a 
+          </a> */}
+          {/* <a 
             className="font-bold text-myred hover:opacity-60"  
             href="https://github.com/delafields"
             target="_blank" rel="noreferrer"
           >
             github
-          </a>
+          </a> */}
         </div>
       </main>
     </div>
